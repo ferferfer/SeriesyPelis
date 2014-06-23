9e13134cf099f2354a8eeaf46706bcf90300591f
ferferfer <ferBook@FerBook.local>
2014-06-23 10:27:16 +0200

    Initial Commit

---
+++ SeriesyPelis/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SeriesyPelis
+//
+//  Created by Fernando Garcia Corrochano on 23/06/14.
+//  Copyright (c) 2014 ironHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SeriesyPelis/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SeriesyPelis
+//
+//  Created by Fernando Garcia Corrochano on 23/06/14.
+//  Copyright (c) 2014 ironHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
